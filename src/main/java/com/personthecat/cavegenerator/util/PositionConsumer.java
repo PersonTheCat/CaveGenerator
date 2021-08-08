9d6e52f21d8920f90bf80df3cb8068db03b5b3c8
PersonTheCat <professionallyperson@gmail.com>
2021-08-07 23:40:25 -0500

    I spent it. It's gone.

--- src/main/java/com/personthecat/cavegenerator/util/PositionConsumer.java
+++
@@ -1,6 +0,0 @@
-package com.personthecat.cavegenerator.util;
-
-@FunctionalInterface
-public interface PositionConsumer {
-    void accept(int x, int y, int z);
-}
--- src/main/java/com/personthecat/cavegenerator/util/PositionPredicate.java
+++
@@ -1,6 +0,0 @@
-package com.personthecat.cavegenerator.util;
-
-@FunctionalInterface
-public interface PositionPredicate {
-    boolean test(int x, int y, int z);
-}
